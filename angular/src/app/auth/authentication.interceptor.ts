d820faa8953a991d45720811224b27344207950b
Timo Stamm <ts@timostamm.de>
2020-02-26 21:14:04 +0100

    Completed authentication interceptor.

--- angular/src/app/auth/authentication.interceptor.ts
+++ angular/src/app/auth/authentication.interceptor.ts
@@ -1,15 +1,22 @@
 import {Injectable} from '@angular/core';
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
+import {EMPTY, Observable, throwError} from 'rxjs';
 import {SessionService} from "./session.service";
 import {environment} from "../../environments/environment";
 import {bearerTokenParse} from "../../lib/bearer-token";
 import {catchError} from "rxjs/operators";
+import {AuthenticationRoutingService} from "./authentication-routing.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
-  constructor(private readonly session: SessionService) {
+  constructor(
+    private readonly session: SessionService,
+    private readonly routing: AuthenticationRoutingService,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly router: Router
+  ) {
   }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
@@ -18,35 +25,28 @@ export class AuthenticationInterceptor implements HttpInterceptor {
 
     return next.handle(request).pipe(
       catchError((response: HttpErrorResponse) => {
-
-        if (response.status !== 401) {
-          // pass error on
-          return throwError(response);
+        if (response.status === 401) {
+          if (this.isExpiredTokenResponse(response)) {
+            // session expired
+            this.session.destroySession();
+            this.routing.sessionExpired(this.router.url);
+            return EMPTY;
+          } else {
+            // token missing or other token error
+            this.session.destroySession();
+            this.routing.login(this.router.url);
+            return EMPTY;
+          }
         }
-
-        // in any case, we have to logout
-        // TODO navigate
-        // this.authService.logout();
-
-        let wwwAuthenticate = bearerTokenParse(response.headers.get('WWW-Authenticate'));
-        if (wwwAuthenticate && wwwAuthenticate.error_description == 'Expired token') {
-
-          // session expired
-          // we might want to prompt for authentication and retry GET request
-          // https://www.learnrxjs.io/operators/error_handling/retrywhen.html
-          // TODO navigate
-          // this.authRouting.navigate401SessionExpired();
-
-        } else {
-
-          // token missing or other token error
-          // TODO navigate
-          // this.authRouting.navigate401Other();
-
-        }
-
+        return throwError(response);
       })
     );
+  }
+
+
+  private isExpiredTokenResponse(response: HttpErrorResponse): boolean {
+    let wwwAuthenticate = bearerTokenParse(response.headers.get('WWW-Authenticate'));
+    return wwwAuthenticate && wwwAuthenticate.error_description == 'Expired token';
   }
 
 
