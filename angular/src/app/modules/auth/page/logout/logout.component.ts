2916bd72957d6f59fcfbda0b5ead0a44f9447a53
Christian Ducrot <christian@ducrot.de>
2020-06-13 16:44:57 +0200

    Beautify login/logout pages

--- angular/src/app/modules/auth/page/logout/logout.component.ts
+++ angular/src/app/modules/auth/page/logout/logout.component.ts
@@ -1,6 +1,8 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {AuthService} from '@app/service/auth.service';
-import {HeaderService} from '@shared/service/header.service';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AuthService } from '@app/service/auth.service';
+import { HeaderService } from '@shared/service/header.service';
+import { ThemeService } from '@app/service/theme.service';
+import { ConstantsService } from '@app/service/constants.service';
 
 @Component({
   selector: 'app-logout',
@@ -10,16 +12,29 @@ import {HeaderService} from '@shared/service/header.service';
 })
 export class LogoutComponent implements OnInit {
 
+  public logo: string;
+  public companyName: string;
+
   constructor(
     private readonly authService: AuthService,
     private headerService: HeaderService,
+    private themeService: ThemeService,
+    private constants: ConstantsService,
+    private cdr: ChangeDetectorRef,
   ) {
     this.headerService.setTitle('Logout');
+    this.companyName = this.constants.companyName;
   }
 
   ngOnInit(): void {
     // always destroy session when the page is opened
     this.authService.destroySession();
+
+    // Select logo for dark/light mode
+    this.themeService.getDarkTheme().subscribe(theme => {
+      this.logo = (theme) ? 'assets/logo-negative.svg' : 'assets/logo.svg';
+      this.cdr.markForCheck();
+    });
   }
 
 }
