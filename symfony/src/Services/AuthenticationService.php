81ab22979597f9fbb28e0996cceb19051747c86b
Christian Ducrot <christian@ducrot.de>
2020-11-08 23:36:57 +0100

    Set user.last_login date after login

--- symfony/src/Services/AuthenticationService.php
+++ symfony/src/Services/AuthenticationService.php
@@ -9,6 +9,7 @@ use App\LoginRequest;
 use App\LoginResponse;
 use App\Security\AuthenticationManager;
 use App\Security\LoginCredentials;
+use Doctrine\ORM\EntityManagerInterface;
 use SymfonyTwirpHandler\TwirpError;
 use Google\Protobuf\Timestamp;
 
@@ -16,14 +17,15 @@ class AuthenticationService implements AuthenticationServiceInterface
 {
 
     private AuthenticationManager $manager;
+    private EntityManagerInterface $em;
 
     /**
      * AuthenticationService constructor.
-     * @param AuthenticationManager $manager
      */
-    public function __construct(AuthenticationManager $manager)
+    public function __construct(AuthenticationManager $manager, EntityManagerInterface $em)
     {
         $this->manager = $manager;
+        $this->em = $em;
     }
 
 
@@ -36,23 +38,31 @@ class AuthenticationService implements AuthenticationServiceInterface
             throw new TwirpError("app.auth.login.missing_parameter", TwirpError::INVALID_ARGUMENT);
         }
 
+        # Validate login credentials
         $credentials = new LoginCredentials(
             $request->getUsername(),
-            $request->getPassword()
+            $request->getPassword(),
+            'local',
+            null
         );
-
         $success = $this->manager->validateLogin($credentials);
         if (!$success) {
             throw new TwirpError("app.auth.login.login_failed", TwirpError::UNAUTHENTICATED);
         }
 
+        // Set last login date
+        $user = $success->getUser();
+        $user->setLastLogin(new \DateTime());
+        $this->em->persist($user);
+        $this->em->flush();
+
+        // Build response
         $response = new LoginResponse();
         $response->setToken($success->getToken());
         $response->setTokenExpiresAt(
             (new Timestamp())->setSeconds($success->getTokenExpiresAt()->getTimestamp())
         );
         $response->setUser($success->getUser()->toProtobuf());
-
         return $response;
     }
 }
