3e3c1cefad22705ee706e4093fc109dfb1b192cc
Christian Ducrot <christian@ducrot.de>
2020-11-08 22:20:44 +0100

    Use PbDatePipe within LocalizedDatePipe

--- angular/src/app/shared/pipe/localized-date.pipe.ts
+++ angular/src/app/shared/pipe/localized-date.pipe.ts
@@ -1,7 +1,13 @@
-import { DatePipe } from '@angular/common';
+import { PbDatePipe } from '@protobuf-ts/runtime-angular';
 import { Pipe, PipeTransform } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+/**
+ * `LocalizedDatePipe` uses the language setting from `ngx-translate` and
+ * `PbDatePipe`, which works exactly like the original angular `DatePipe`,
+ * but also understands `google.protobuf.Timestamp` and `google.type.DateTime`.
+ * @link https://angular.io/api/common/DatePipe
+ */
 @Pipe({
   name: 'localizedDate',
   pure: false
@@ -11,7 +17,7 @@ export class LocalizedDatePipe implements PipeTransform {
   }
 
   transform(value: any, pattern: string = 'mediumDate'): any {
-    const datePipe: DatePipe = new DatePipe(this.translateService.currentLang);
-    return datePipe.transform(value, pattern);
+    const pbDatePipe: PbDatePipe = new PbDatePipe(this.translateService.currentLang);
+    return pbDatePipe.transform(value, pattern);
   }
 }
