4b5f2652359bb2fb8db2e3ba3439bdaa8010747c
Christian Ducrot <christian@ducrot.de>
2024-07-07 14:18:28 +0200

    Refactor tests to use assertSame instead of assertEquals

--- symfony/tests/Controller/AuthenticationControllerTest.php
+++ symfony/tests/Controller/AuthenticationControllerTest.php
@@ -13,8 +13,7 @@ class AuthenticationControllerTest extends WebTestCase
 {
     use RefreshDatabaseTrait;
 
-    /** @var KernelBrowserAlias */
-    private $client;
+    private KernelBrowserAlias $client;
 
     protected function setUp(): void
     {
@@ -24,28 +23,28 @@ class AuthenticationControllerTest extends WebTestCase
     public function testMethodGetNotAllowed()
     {
         $this->client->request('GET', '/api/app.AuthenticationService/login');
-        $this->assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
+        $this->assertSame(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode(), $this->client->getResponse()->getContent());
         $this->assertJson($this->client->getResponse()->getContent());
         $response = json_decode($this->client->getResponse()->getContent());
-        $this->assertEquals('Method GET not allowed.', $response->msg);
+        $this->assertSame('Method GET not allowed.', $response->msg);
     }
 
     public function testMissingContentType()
     {
         $this->client->request('POST', '/api/app.AuthenticationService/login');
-        $this->assertEquals(Response::HTTP_BAD_REQUEST, $this->client->getResponse()->getStatusCode());
+        $this->assertSame(Response::HTTP_BAD_REQUEST, $this->client->getResponse()->getStatusCode(), $this->client->getResponse()->getContent());
         $this->assertJson($this->client->getResponse()->getContent());
         $response = json_decode($this->client->getResponse()->getContent());
-        $this->assertEquals('Missing content-type application/protobuf or application/json', $response->msg);
+        $this->assertSame('Missing content-type application/protobuf or application/json', $response->msg);
     }
 
     public function testLoginEmptyCredentials()
     {
         $this->client->request('POST', '/api/app.AuthenticationService/login', [], [], ['CONTENT_TYPE' => 'application/json'], '{}');
-        $this->assertEquals(Response::HTTP_BAD_REQUEST, $this->client->getResponse()->getStatusCode());
+        $this->assertSame(Response::HTTP_BAD_REQUEST, $this->client->getResponse()->getStatusCode(), $this->client->getResponse()->getContent());
         $this->assertJson($this->client->getResponse()->getContent());
         $response = json_decode($this->client->getResponse()->getContent());
-        $this->assertEquals('app.form.missing_parameters', $response->msg);
+        $this->assertSame('app.form.missing_parameters', $response->msg);
     }
 
     public function testLoginWrongUsername()
@@ -57,10 +56,10 @@ class AuthenticationControllerTest extends WebTestCase
         $content = $loginRequest->serializeToString();
 
         $this->client->request('POST', '/api/app.AuthenticationService/login', [], [], ['CONTENT_TYPE' => 'application/protobuf'], $content);
-        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $this->client->getResponse()->getStatusCode());
+        $this->assertSame(Response::HTTP_UNAUTHORIZED, $this->client->getResponse()->getStatusCode(), $this->client->getResponse()->getContent());
         $this->assertJson($this->client->getResponse()->getContent());
         $response = json_decode($this->client->getResponse()->getContent());
-        $this->assertEquals('app.auth.login.login_failed', $response->msg);
+        $this->assertSame('app.auth.login.login_failed', $response->msg);
     }
 
     public function testLoginWrongPassword()
@@ -72,10 +71,10 @@ class AuthenticationControllerTest extends WebTestCase
         $content = $loginRequest->serializeToString();
 
         $this->client->request('POST', '/api/app.AuthenticationService/login', [], [], ['CONTENT_TYPE' => 'application/protobuf'], $content);
-        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $this->client->getResponse()->getStatusCode());
+        $this->assertSame(Response::HTTP_UNAUTHORIZED, $this->client->getResponse()->getStatusCode(), $this->client->getResponse()->getContent());
         $this->assertJson($this->client->getResponse()->getContent());
         $response = json_decode($this->client->getResponse()->getContent());
-        $this->assertEquals('app.auth.login.login_failed', $response->msg);
+        $this->assertSame('app.auth.login.login_failed', $response->msg);
     }
 
     public function testLoginSucess()
@@ -87,11 +86,11 @@ class AuthenticationControllerTest extends WebTestCase
         $content = $loginRequest->serializeToString();
 
         $this->client->request('POST', '/api/app.AuthenticationService/login', [], [], ['CONTENT_TYPE' => 'application/protobuf'], $content);
-        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode(), $this->client->getResponse()->getContent());
         $this->assertIsString($this->client->getResponse()->getContent());
 
         $response = new LoginResponse();
         $response->mergeFromString($this->client->getResponse()->getContent());
-        $this->assertEquals('testuser@domain.tld', $response->getUser()->getUsername());
+        $this->assertSame('testuser@domain.tld', $response->getUser()->getUsername());
     }
 }
